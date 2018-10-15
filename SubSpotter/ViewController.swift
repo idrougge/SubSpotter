b54afcc7490cbfceab87f09d09fd29581e45fe15
Iggy Drougge <idrougge@mac.com>
2018-10-15 14:07:28 +0200

    Initial Commit

---
+++ SubSpotter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SubSpotter
+//
+//  Created by Iggy Drougge on 2018-10-15.
+//  Copyright © 2018 Iggy Drougge. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
